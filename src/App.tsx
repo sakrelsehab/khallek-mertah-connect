1e1ae260745d2d5210efec97652bd572738e64e2
sakrelsehab <sakr.elsehab@gmail.com>
2025-09-19 04:59:00 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -66,3 +66,29 @@ function App() {
 }
 
 export default App;
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import Auth from "./pages/Auth";
+import Dashboard from "./pages/Dashboard";
+import DeliveryService from "./pages/DeliveryService";
+import DeliveryServiceReal from "./pages/DeliveryServiceReal";
+import RealEstate from "./pages/RealEstate";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/delivery" element={<DeliveryService />} />
+        <Route path="/real-estate" element={<RealEstate />} />
+        <Route path="/equipment" element={<DeliveryServiceReal />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
